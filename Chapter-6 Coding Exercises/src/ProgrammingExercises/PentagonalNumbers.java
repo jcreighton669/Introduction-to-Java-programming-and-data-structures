576ec66a98e2baf884a710d4b959971f7e457d49
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2021-01-18 09:57:26 -0500

    Update PentagonalNumbers.java

--- Chapter-6 Coding Exercises/src/ProgrammingExercises/PentagonalNumbers.java
+++ Chapter-6 Coding Exercises/src/ProgrammingExercises/PentagonalNumbers.java
@@ -10,5 +10,14 @@ package ProgrammingExercises;
  * @author Justin
  */
 public class PentagonalNumbers {
-	
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(getPentagonalNumber(i) + " ");
+		}
+	}
+
+	public static int getPentagonalNumber(int n) {
+		return (n * (3 * n - 1)) / 2;
+	}
 }
