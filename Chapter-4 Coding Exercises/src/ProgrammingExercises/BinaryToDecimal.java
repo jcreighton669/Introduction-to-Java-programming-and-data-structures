c8d9ac98e34ff392025bcf000d3b584223bbd125
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2021-01-19 15:44:07 -0500

    Update BinaryToDecimal.java

--- Chapter-4 Coding Exercises/src/ProgrammingExercises/BinaryToDecimal.java
+++ Chapter-4 Coding Exercises/src/ProgrammingExercises/BinaryToDecimal.java
@@ -5,10 +5,38 @@
  */
 package ProgrammingExercises;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Justin
  */
 public class BinaryToDecimal {
-	
+
+	/**
+	 * Main method
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter binary digits (0000 to 1111): ");
+		int num = input.nextInt();
+		int dec_value = 0;
+
+		int base = 1;
+		int temp = num;
+
+		while (temp > 0) {
+			int last_digit = temp % 10;
+			temp = temp / 10;
+
+			dec_value += last_digit * base;
+
+			base = base * 2;
+		}
+
+		System.out.println("The decimal value is " + dec_value);
+	}
 }
