21c7762e46750b11a60b9541a68aa99bcf6d7d2e
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2021-01-20 12:26:42 -0500

    Update MonetaryUnits.java

--- Chapter-4 Coding Exercises/src/ProgrammingExercises/MonetaryUnits.java
+++ Chapter-4 Coding Exercises/src/ProgrammingExercises/MonetaryUnits.java
@@ -12,17 +12,23 @@ import java.util.Scanner;
  * @author Justin
  */
 public class MonetaryUnits {
+
+	/**
+	 * Main method
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Enter an amount in double for example 11.56: ");
 		String money = input.nextLine();
-		
+
 		int dollars = Integer.parseInt(money.substring(0, money.indexOf(".")));
-		int cents = Integer.parseInt(money.substring(money.indexOf(".")+1));
-		
+		int cents = Integer.parseInt(money.substring(money.indexOf(".") + 1));
+
 		System.out.println("Your amount " + money + " consists of " + dollars + " dollars and " + cents + " cents");
-		
+
 		input.close();
 	}
 }
