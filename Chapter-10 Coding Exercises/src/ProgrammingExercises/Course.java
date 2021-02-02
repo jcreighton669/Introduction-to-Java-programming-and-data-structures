429c5e7467fda6adbef2e17345ed55d397cef3e1
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2021-02-02 15:19:59 -0500

    Create Course.java

---
+++ Chapter-10 Coding Exercises/src/ProgrammingExercises/Course.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProgrammingExercises;
+
+/**
+ *
+ * @author Justin
+ */
+public class Course {
+
+	private String courseName;
+	private String[] students = new String[100];
+	private int numberOfStudents;
+
+	public Course(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public void addStudent(String student) {
+		students[numberOfStudents] = student;
+		numberOfStudents++;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public String[] getStudents() {
+		return students;
+	}
+
+	public int getNumberOfStudents() {
+		return numberOfStudents;
+	}
+
+	public void dropStudent(String student) {
+		// Left as an exercise in Programming Exercise 10.9
+	}
+}
