bdf2ae44b0dfeda76d449ff324128dbc7fe15e2b
Justin Allen Creighton <46484674+jcreighton669@users.noreply.github.com>
2021-02-03 10:02:11 -0500

    Update Course.java

--- Chapter-10 Coding Exercises/src/ProgrammingExercises/Course.java
+++ Chapter-10 Coding Exercises/src/ProgrammingExercises/Course.java
@@ -20,6 +20,13 @@ public class Course {
 	}
 
 	public void addStudent(String student) {
+		if (numberOfStudents == students.length) {
+			String[] a = new String[students.length + 1];
+			for (int i = 0; i < numberOfStudents; i++) {
+				a[i] = students[i];
+			}
+			students = a;
+		}
 		students[numberOfStudents] = student;
 		numberOfStudents++;
 	}
@@ -37,6 +44,32 @@ public class Course {
 	}
 
 	public void dropStudent(String student) {
-		// Left as an exercise in Programming Exercise 10.9
+		int index = findStudent(student);
+		if (index >= 0) {
+			dropStudent(index);
+		} else {
+			System.out.println(student + " is not in the course: " + courseName);
+		}
+	}
+
+	private void dropStudent(int index) {
+		String[] s = new String[students.length - 1];
+		for (int i = 0, j = 0; i < s.length; i++, j++) {
+			if (i == index) {
+				j++;
+			}
+			s[i] = students[j];
+		}
+		this.students = s;
+		numberOfStudents--;
+	}
+
+	private int findStudent(String student) {
+		for (int i = 0; i < numberOfStudents; i++) {
+			if (students[i].equals(student)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
